de2ef0546533c4ab3b392b7175a09242348235d0
Jacob Klimenko <droidedjake@gmail.com>
2021-03-21 15:56:57 -0400

    Set up views for growing zones

--- app/controllers/growing_zones_controller.rb
+++ app/controllers/growing_zones_controller.rb
@@ -2,7 +2,11 @@
 class GrowingZonesController < ApplicationController
 
     def index
-        @growing_zones = GrowingZone.all
+        @growing_zones = GrowingZone.order(:created_at)
+    end
+
+    def show
+        @growing_zone = GrowingZone.find_by(params[:id])
     end
 
 end
